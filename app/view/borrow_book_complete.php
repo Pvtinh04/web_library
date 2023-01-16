fb5b9c27b9ee3776c90c2fc96e38fed129e67bd2
Pham Tinh <tylh.sweet.311@gmail.com>
2023-01-16 18:02:29 +0700

    fix

--- app/view/borrow_book_complete.php
+++ app/view/borrow_book_complete.php
@@ -7,7 +7,7 @@
 
         <div style="display: flex; width: 450px; justify-content: space-around; margin: 10px auto">
             <label style="  align-items: center; font-size: 16px">
-        <a href="index.php?page=home">Quay lại danh sách</a>
+        <a href="index.php?page=home">Quay lại trang chủ</a>
         </label>
         </div>
 
--- app/view/borrow_book_input.php
+++ app/view/borrow_book_input.php
@@ -1,4 +1,4 @@
-
+<h1 class="text-center">Thêm mới mượn sách</h1>
 <div class="container" style="height: 550px; width: 550px; margin: 15px auto; border: 1px solid #5b9bd5; display: flex; justify-content:center; flex-direction: column; align-items: center;">
 <form method="post" enctype="multipart/form-data" action="">
 <!-- name -->
